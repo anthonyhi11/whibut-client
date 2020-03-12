f317e6c5cedad2d98454902e83bfbfc7359834e2
Anthony Hill <antdavhill@gmail.com>
2020-03-12 13:27:56 -0500

    mobile design done with moviedash and main, messed up desktop sizing

--- src/Dashboards/MoviesDashboard.js
+++ src/Dashboards/MoviesDashboard.js
@@ -35,20 +35,20 @@ componentDidMount() {
               />
     })
     return (
-      <div>
+      <div className='activity-dash-container'>
         <Navbar history={this.props.history}/>
         <Footer history={this.props.history}/>
         <div className='activity-dash'>
-          <div className='activity-dash-content'>
+        <div className='activity-add-div'>
           <h1>Movies</h1>
-            <button className='activity-add-button' onClick={this.handleClick}>+</button>
-            <p className='add-text' onClick={this.handleClick}>Add Movie</p>
-            <div className='randomdiv'>
-              {this.context.isAddActive && <MovieForm history={this.props.history}/>}
-            </div>
-          <section className='activity-results'>
+          <button className='activity-add-button' onClick={this.handleClick}>+</button>
+          <p className='add-text' onClick={this.handleClick}>Add Movie</p>
+          </div>
+          <div className='randomdiv'>
+            {this.context.isAddActive && <MovieForm history={this.props.history}/>}
+          </div>
+          <div className='activity-results'>
             {results}
-          </section>
           </div>
         </div>
       </div>
