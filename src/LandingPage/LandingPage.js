1c094d0ec4f0e0a57e55ad1c0d709cbd0f3f686e
Anthony Hill <antdavhill@gmail.com>
2020-05-13 17:08:12 -0500

    changed up landing page to include demo log in from front screen and changed heirarchy. Initial design edits, post design audit.

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,6 +4,8 @@ import SignUp from './SignUp/SignUp';
 import './landingpage.css';
 import WhibutContext from '../WhibutContext';
 import HowToUse from './HowToUse/HowToUse';
+import LogInSignUpService from '../services/login-signup-service';
+import TokenService from '../services/token-service';
 
 export default class LandingPage extends React.Component {
   constructor(props) {
@@ -12,6 +14,7 @@ export default class LandingPage extends React.Component {
       errorActive: false,
       error: null,
       modalShown: false,
+      successShown: false,
     };
   }
 
@@ -31,29 +34,59 @@ export default class LandingPage extends React.Component {
     });
   };
 
+  handleDemo = (e) => {
+    e.preventDefault();
+    this.setState({ successShown: true });
+
+    const loginAttempt = {
+      username: 'demouser',
+      password: 'AAaa11!!',
+    };
+    LogInSignUpService.loginUser(loginAttempt)
+      .then((res) => {
+        TokenService.saveAuthToken(res.authToken);
+        this.context.handleCancel();
+        this.handleSubmit(); //routes you to the main dashboard
+      })
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
+  };
+
   static contextType = WhibutContext;
   render() {
+    const { successShown } = this.state;
     return (
       <div className="landing-page-main">
+        {successShown && (
+          <div className="success-screen">
+            <h3>Loading</h3>
+            <p>This application is hosted on Heroku with a free plan. It may take a few seconds to spin up.</p>
+          </div>
+        )}
         <header>
           <a href="/">
             <img src="./images/whibyt-mark1.png" className="hero-img" alt="whibut logo"></img>
           </a>
         </header>
+        <p className="howtouse-button" onClick={this.handleHow}>
+          How to use?
+        </p>
         <video height="300px" width="450px" controls className="video">
           <source src="./images/whibut.mp4" type="video/mp4" />
         </video>
-        <button className="howtouse-button" onClick={this.handleHow}>
-          How to use?
-        </button>
+
         {this.state.modalShown && <HowToUse handleCancel={this.handleCancel} />}
         <div className="signinbuttons">
+          <button onClick={this.context.handleLoginClick} className="login-button">
+            Log In
+          </button>
           <button onClick={this.context.handleSignupClick} className="signup-button">
             Sign Up
           </button>
           {this.context.signupActive && <SignUp handleSubmit={this.handleSubmit} />}
-          <p onClick={this.context.handleLoginClick} className="login-button">
-            Log in
+          <p onClick={this.handleDemo} className="demo-button">
+            Demo
           </p>
           {this.context.loginActive && <LogIn handleSubmit={this.handleSubmit} />}
         </div>
--- src/LandingPage/LogIn/LogIn.js
+++ src/LandingPage/LogIn/LogIn.js
@@ -7,7 +7,6 @@ import './login.css';
 export default class LogIn extends React.Component {
   state = {
     error: null,
-    successShown: false,
   };
   handleLogIn = (e) => {
     e.preventDefault();
@@ -26,44 +25,14 @@ export default class LogIn extends React.Component {
       });
   };
 
-  handleDemo = (e) => {
-    e.preventDefault();
-    this.setState({ successShown: true });
-
-    const loginAttempt = {
-      username: 'demouser',
-      password: 'AAaa11!!',
-    };
-    LogInSignUpService.loginUser(loginAttempt)
-      .then((res) => {
-        TokenService.saveAuthToken(res.authToken);
-        this.context.handleCancel();
-        this.props.handleSubmit(); //routes you to the main dashboard
-      })
-      .catch((res) => {
-        this.setState({ error: res.error });
-      });
-  };
-
   static contextType = WhibutContext;
   render() {
     const { error } = this.state;
-    const { successShown } = this.state;
+
     return (
       <div className="login-modal">
-        {successShown && (
-          <div className="success-screen">
-            <p>Success!</p>
-          </div>
-        )}
         <div role="alert">{error && <p className="login-error">{error}</p>}</div>
-        <div className="demo-creds-div">
-          <button className="login-button-small" type="submit" onClick={(e) => this.handleDemo(e)}>
-            DEMO USER
-          </button>
-          {/* <p className={error ? 'error-creds': 'demo-creds'}>demo username: demouser</p>
-              <p className={error ? 'error-creds': 'demo-creds'}>demo password: AAaa11!!</p> */}
-        </div>
+
         <form className={error ? 'error-form' : 'login-form'} onSubmit={(e) => this.handleLogIn(e)}>
           <label htmlFor="name">Username</label>
           <input type="text" id="name" name="name" placeholder="Username" />
